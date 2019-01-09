10ce407f435f9e9d3efe9c06452ae7c406c58821
vidhurraj147 <vidhurraj147@gmail.com>
2019-01-09 01:25:06 -0500

    chmod 777 executed

--- src/app/address-card/address-card.component.ts
+++ src/app/address-card/address-card.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddressCardComponent implements OnInit {
 
+  user = {
+    name: "Foo Bar",
+    title: "Software Developer",
+    address: "1234 Mainst, City, State 500013",
+    phone: [
+              123-456-789,
+              987-654-852
+            ]
+  };
+
   constructor() { }
 
   ngOnInit() {
