ddcea5a70ccc9e827ca6a276a5dc01433243a282
vidhurraj147 <vidhurraj147@gmail.com>
2019-01-09 19:31:04 -0500

    changes and configuration for FAILURE routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SettingsComponent } from './settings/settings.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 // const routes: Routes = [];
@@ -11,6 +12,7 @@ const routes: Route[] = [
   { path: '' , redirectTo: '/home', pathMatch: 'full'}, //  pathMatch: 'prefix'
   { path: 'home', component: HomeComponent },
   { path: 'settings', component: SettingsComponent },
+  { path:'**', component: PageNotFoundComponent }
   // { path: 'view2', component: 'componentName' },
   // { path: 'view3', component: 'componentName' }
 ];
